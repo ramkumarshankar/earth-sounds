be5dd748c5e4531f23c37d05d3464d5a57ec50ed
Ramkumar Shankar <ramkumarshankr@gmail.com>
2016-12-22 17:10:32 +0530

    keep class in index.js

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,8 +1,42 @@
 var earthquakes = [];
 var textFont;
 
-//Todo: use moment to work with the time values
-//Values already arrive in descending order
+class Earthquake {
+    constructor(id, title, mag, long, lat, depth, time) {
+        //Get our values
+        this._id = id;
+        this._title = title;
+        this._mag = mag;
+        this._long = long;
+        this._lat = lat;
+        this._depth = depth;
+        this._time = time;
+
+        //Initialise the oscillator for this event
+        this._osc = new p5.Oscillator(440, 'sine');
+        this._osc.amp(0);
+        this._osc.start();
+        this._isPlaying = false;
+    }
+
+    play() {
+        this._isPlaying = true;
+        this._osc.amp(0.5, 0.05);
+    }
+
+    stop() {
+        this._isPlaying = false;
+        this._osc.amp(0, 0.5);
+    }
+
+    draw() {
+        var x = map(this._long, -180, 180, 0, width);
+        var y = map(this._lat, -90, 90, 0, height);
+        color(255, 255, 255, 0.1);
+        noStroke();
+        ellipse(x, y, 10, 10);
+    }
+}
 
 function preload() {
     textFont = loadFont('./assets/Chivo-Light.otf');
@@ -11,6 +45,7 @@ function preload() {
 }
 
 function loadData(results) {
+    var firstEventTime = moment(results.features[0].time);
     for (var i = 0; i < results.features.length; i++) {
         let quakeEvent = new Earthquake(
             i,
@@ -18,7 +53,8 @@ function loadData(results) {
             results.features[i].properties.mag,
             results.features[i].geometry.coordinates[0],
             results.features[i].geometry.coordinates[1],
-            results.features[i].geometry.coordinates[2]
+            results.features[i].geometry.coordinates[2],
+            moment(results.features[i].time).diff(firstEventTime, 'seconds')
         );
         quakeEvent._osc.stop();
         earthquakes.push(quakeEvent);
@@ -35,8 +71,6 @@ function draw() {
     noStroke();
     color(255,255,255);
     for (var i = 0; i < earthquakes.length; i++) {
-        var x = map(earthquakes[i]._long, -180, 180, 0, width);
-        var y = map(earthquakes[i]._lat, -90, 90, 0, height);
-        ellipse(x, y, 20, 20);
+        earthquakes[i].draw();
     }
 }
