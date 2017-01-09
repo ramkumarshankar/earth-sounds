8ce50eac68c8fddd746de86e5aae1b40f2878473
Ramkumar Shankar <ramkumarshankr@gmail.com>
2017-01-09 13:06:29 +1100

    add scroll for overlay is content spills over
    hide trigger button in overlay page

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -3,10 +3,12 @@ function toggleOverlay() {
     if (document.querySelector('.open') == null) {
         overlay.classList.add('open');
         overlay.classList.remove('close');
+        triggerButton.classList.add('hide');
     }
     else {
         overlay.classList.remove('open');
         overlay.classList.add('close');
+        triggerButton.classList.remove('hide');
     }
 }
 
